27f332df1976141e8dcc3810127a2fde4555080c
kishore kumar <kishorekumar.sapui@gmail.com>
2023-01-30 10:40:37 +0000

    label5

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("poitem.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
